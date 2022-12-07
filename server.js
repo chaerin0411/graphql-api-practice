4d25b7dc15cdbeb30ddf0027d151c693724141eb
jeonchaerin <chaerin0411@naver.com>
2022-12-07 15:47:24 +0900

    feat: Add query resolvers

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 import { ApolloServer, gql } from "apollo-server";
 
+const tweets = [
+	{
+		id: "1",
+		text: "first one",
+	},
+	{
+		id: "2",
+		text: "second one",
+	},
+];
+
 const typeDefs = gql`
 	type User {
 		id: ID!
 		username: String!
-        firstName: String!
-        lastName: String!
+		firstName: String!
+		lastName: String!
 	}
 	type Tweet {
 		id: ID!
@@ -21,7 +32,19 @@ const typeDefs = gql`
 		deleteTweet(id: ID!): Boolean!
 	}
 `;
-const server = new ApolloServer({ typeDefs });
+
+const resolvers = {
+	Query: {
+		allTweets() {
+			return tweets;
+		},
+		tweet(root, { id }) {
+			return tweets.find((tweet) => tweet.id === id);
+		},
+	},
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
 	console.log(`Running on ${url}`);
